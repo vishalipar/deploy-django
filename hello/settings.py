0718a9ad4d9e9835e0c69cf5a0ec22869be70296
vishalipar <vishalipar04@gmail.com>
2024-11-30 17:58:21 +0530

    Create superuser

--- hello/settings.py
+++ hello/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3y2sqtr0oz(400ab83t87m1tp*n)a58)%54f756locwd1sxf)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
